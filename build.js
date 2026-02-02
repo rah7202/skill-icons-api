61e19ed415d77d3d934b53ca584d8f8da1b47461
rah7202 <rpidiyar249@gmail.com>
2026-02-03 02:41:25 +0530

    Initial commit: SVG icon generator API using Cloudflare Workers

---
+++ build.js
@@ -0,0 +1,13 @@
+import fs from "fs"
+
+const iconsDir = fs.readdirSync('./icons');
+const icons = {};
+for (const icon of iconsDir) {
+    const name = icon.replace('.svg', '').toLowerCase();
+    icons[name] = String(fs.readFileSync(`./icons/${icon}`));
+}
+
+if (!fs.existsSync('./dist')) fs.mkdirSync('./dist');
+fs.writeFileSync('./dist/icons.json', JSON.stringify(icons));
+
+console.log("icons.json generated");
